34cf309b1fb956151d950f9af21f37699a40b65c
Kar Yin Ng <karyin3@uw.edu>
2018-05-17 21:35:06 -0700

    work in progress

---
+++ CurrentDonations.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import React, {Component} from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import firebase from 'firebase';
+
+export default class CurrentDonations extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    
+
+    render() {
+        return(
+
+        );
+    }
+}
